c775d73b00bb527578b9f271a24b8582d799339a
Irina <irbris@mail.ru>
2019-07-17 13:20:18 +0300

    feat: react-slider

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Slider from './components/Slider';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Slider />
+    )
+  }
+}
+
+export default App;
